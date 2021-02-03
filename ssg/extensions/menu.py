65ce01bf74614489dddd588803384ef70b0d2539
yangxuedong <50256207+yangxuedong@users.noreply.github.com>
2021-02-02 19:00:09 -0600

    Update menu.py

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -18,4 +18,4 @@ def generate_menu(html, ext):
     template = '<li><a href="{}{}">{}</a></li>'
     menu_item = lambda name, ext: template.format(name, ext, name.title())
     menu = "\n".join([menu_item(path.stem, ext) for path in files])
-    return "<ul>\n{}<ul>\n{}".format(menu, html)
+    return "<ul>\n{}</ul>\n{}".format(menu, html)
