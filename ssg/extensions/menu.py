ed0d68795a4ce0ff546b55bf4b8ee43f6132676e
yangxuedong <50256207+yangxuedong@users.noreply.github.com>
2021-02-02 18:40:51 -0600

    fix error

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -6,12 +6,11 @@ files = []
 
 @hooks.register("collect_files")
 def collect_files(source, site_parsers):
-    valid = lambda p: p.isinstance(parsers.ResourceParser)
+    valid = lambda p: not isinstance(p, parsers.ResourceParser)
     for path in source.rglob("*"):
-        for parser in list(filter(valid, path)):
+        for parser in list(filter(valid, site_parsers)):
             if parser.valid_file_ext(path.suffix):
                 files.append(path)
-    return not valid
 
 
 @hooks.register("generate_menu")
