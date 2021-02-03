5adb6c0591639f80802c1a3c5c4803513d2de1d7
yangxuedong <50256207+yangxuedong@users.noreply.github.com>
2021-02-02 20:34:54 -0600

    Update stats.py

--- ssg/extensions/stats.py
+++ ssg/extensions/stats.py
@@ -21,5 +21,5 @@ def written():
 def stats():
     final_time = time.time() - start_time
     average = final_time / total_written if total_written else 0
-    report = "Converted: {} · Time: {:.2f} sec · Avg: {:,4f} sec/file"
+    report = "Converted: {} · Time: {:.2f} sec · Avg: {:.4f} sec/file"
     print(report.format(total_written, final_time, average))
