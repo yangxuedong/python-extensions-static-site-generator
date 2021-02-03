414bebc305d0740679e6777498a1886fcd891f9a
yangxuedong <50256207+yangxuedong@users.noreply.github.com>
2021-02-02 18:57:46 -0600

    Update menu.py

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -19,8 +19,3 @@ def generate_menu(html, ext):
     menu_item = lambda name, ext: template.format(name, ext, name.title())
     menu = "\n".join([menu_item(path.stem, ext) for path in files])
     return "<ul>\n{}<ul>\n{}".format(menu, html)
-
-
-
-
-
